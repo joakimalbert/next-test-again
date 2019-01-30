c88e25d317b5f79a9c3b01b590923fb514e89694
Joakim Johansson <joakim@hejalbert.se>
2019-01-30 17:29:16 +0100

    Added images

--- components/Landing/USP.js
+++ components/Landing/USP.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 class USP extends React.Component {
     render() {
         return (
@@ -7,14 +5,17 @@ class USP extends React.Component {
                 <div className="container">
                     <div>
                         <h2>Research-based content</h2>
+                        <h2 className="usp__title">Research-based content</h2>
                         <p>Content based on years of research developed by highly experienced teachers make sure your child learns the right things in the right order. All of it.</p>
                     </div>
                     <div>
                         <h2>Interactive excercises</h2>
+                        <h2 className="usp__title">Interactive excercises</h2>
                         <p>The interactive way of solving exercises will help your child understand and learn mathematics with a high level of enthusiasm and engagement enhancing the learning experience.</p>
                     </div>
                     <div>
                         <h2>Gamified experience</h2>
+                        <h2 className="usp__title">Gamified experience</h2>
                         <p>Gamified learning journey with unlocking of new levels, challenges and continuous positive feedbck from now on bring great value of child's screentime.</p>
                     </div>
                 </div>
@@ -22,6 +23,13 @@ class USP extends React.Component {
                     .usp__container {
                         background-color: #F6F5F9;
                         margin-top: 100px;
+                    }
+                    .usp__title {
+                        font-size: 4.5rem;
+                        font-weight: bold;
+                    }
+                    .usp__content {
+                        
                     }
             `}</style>
             </div>
